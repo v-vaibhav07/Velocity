a8ab438ea5cb1d73da89460c7390d2ce3471e0a5
Vaibhav Yadav <vaibhavyadav6533@gmail.com>
2026-06-15 13:45:44 +0530

    Set up MongoDB database connection

---
+++ rydex/src/lib/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+
+import dns from "dns"
+
+
+dns.setServers([
+      '1.1.1.1',
+      '8.8.8.8'
+])
+
+const mongodbUrl=process.env.MONGODB_URL
+
+if(!mongodbUrl){
+    throw new Error("db url not found!")
+}
+
+let cached=global.mongooseConn
+if(!cached){
+    cached=global.mongooseConn={conn:null,promise:null}
+}
+
+const connectDb=async () => {
+    if(cached.conn){
+        return cached.conn
+    }
+
+
+    if(!cached.promise){
+        cached.promise=mongoose.connect(mongodbUrl).then(c=>c.connection)
+    }
+
+try {
+    const conn=await cached.promise
+  
+    return conn
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+export default connectDb
