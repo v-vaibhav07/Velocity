514c461deb1496fc14daae9ecfae8d7fb3233832
Vaibhav Yadav <vaibhavyadav6533@gmail.com>
2026-06-29 16:42:52 +0530

    u

--- rydex/src/app/api/admin/video-kyc/start/[id]/route.ts
+++ rydex/src/app/api/admin/video-kyc/start/[id]/route.ts
@@ -26,72 +26,6 @@ export async function GET(
                 {status:400}
             )
         } 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export async function GET(
-//      req: NextRequest,
-//     context: { params: Promise<{ id: string }>}
-// ) {
-//     try {
-//             const session = await auth()
-//         if (!session || !session.user?.email || session.user.role !== "admin") {
-//             return Response.json({ message: "unauthorized" }
-//                 , { status: 400 }
-//             )
-//         }
-
-//         await connectDb()
-//         const partnerId=(await context.params).id
-//         const partner=await User.findById(partnerId)
-
-//         if(!partner || partner.role!=="partner"){
-//             return Response.json(
-//                 {message:"partner not found"},
-//                 {status:400}
-//             )
-//         } 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
        const roomId=`kyc-${partner._id}-${Date.now()}` 
        partner.videoKycRoomId=roomId
@@ -99,45 +33,6 @@ export async function GET(
        partner.partnerOnBoardingSteps=4
 
        await partner.save()
-
-
-
-
-
-
-// export async function GET(
-//      req: NextRequest,
-//     context: { params: Promise<{ id: string }>}
-// ) {
-//     try {
-//             const session = await auth()
-//         if (!session || !session.user?.email || session.user.role !== "admin") {
-//             return Response.json({ message: "unauthorized" }
-//                 , { status: 400 }
-//             )
-//         }
-
-//         await connectDb()
-//         const partnerId=(await context.params).id
-//         const partner=await User.findById(partnerId)
-
-//         if(!partner || partner.role!=="partner"){
-//             return Response.json(
-//                 {message:"partner not found"},
-//                 {status:400}
-//             )
-//         } 
-
-
-
-
-
-
-
-
-
-
-
        return NextResponse.json({roomId})
     } catch (error) {
          return NextResponse.json({message:"video kyc start error"},{status:500})
