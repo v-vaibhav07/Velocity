defe384994f9ff804937c4539755b09f6b7a4b80
Vaibhav Yadav <vaibhavyadav6533@gmail.com>
2026-07-10 10:04:39 +0530

    Add useGetMe hook

--- rydex/src/hooks/useGetMe.tsx
+++ rydex/src/hooks/useGetMe.tsx
@@ -20,38 +20,6 @@ const getMe=async ()=>{
     }
  
 }
-
-
-
-
-
-
-// function useGetMe(enabled:boolean) {
-//    const dispatch=useDispatch()
-   
-//  useEffect(()=>{
-//      if(!enabled){
-//         return
-//     }
-// const getMe=async ()=>{
-//     try {
-//     const {data}=await axios.get("/api/user/me")
-//     dispatch(setUserData(data))
-//     } catch (error) {
-//         console.log(error)
-//     }
- 
-// }
-
-
-
-
-
-
-
-
-
-
 getMe()
  },[enabled])
 }
